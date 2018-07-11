b3bc2f82e5ecdd0733b960761b1e4a9f496999ab
darkmastermindz <darkmastermindz@gmail.com>
2018-07-11 01:46:57 -0400

    - Added Report Command - Added Kick Command - Added Ban Command - Setting up modularity within commands.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const botconfig = require("./botconfig.json");
 const token = require("./token.json"); // Create a json file with {value: yourToken}
 const Discord = require("discord.js");
+const express = require('express');
+const fs = requre("fs");
 
 const bot = new Discord.Client({disableEveryone: true})
+
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) =? {
+  if (err) console(err);
+
+  let jsfile = files.filter(f => f.split(".").pop ==== "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.")
+    return;
+  }
+  console.log("loading commands");
+  js.file.forEach((f, i) = +> {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set props.help.name, props
+  });
+});
 
 bot.on("ready", async () => {
   console.log('${bot.user.username} is online!')
@@ -36,6 +56,7 @@ bot.on("message", async message => {
         .addField("botinfo", "get bot info")
         .addField("serverinfo", "get bot info")
         .addField("report", "report a user")
+        .addField("report", "report a user");
       return message.channel.send(helpEmbed);
 
     case `hello`:
@@ -50,8 +71,7 @@ bot.on("message", async message => {
         .addField("Bot Name", bot.user.username)
         .addField("Created On", bot.user.createdAt);
 
-      return message.channel.send(botEmbed);
-
+      return message.channel.send(botEmbed)
 
     case `serverinfo`:
       let serverIcon = message.guild.displayAvatarURL
@@ -66,21 +86,19 @@ bot.on("message", async message => {
 
       return message.channel.send(serverEmbed);
 
-      //** Administrative Commands **//
-
       case `report`:
-        let rUser = message.guild.member(message.mentions.users.first())
+        let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!rUser) return message.channel.send("Couldn't find user.");
         let reason = args.join(" ").slice(22);
 
         let reportEmbed = new Discord.RichEmbed()
-        .setDescription("Reports")
-        .setColor("E5DA2A")
-        .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-        .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-        .addField("Channel", message.channel)
-        .addField("Time", message.createdAt)
-        .addField("Reason", reason);
+          .setDescription("Reports")
+          .setColor("#E5DA2A")
+          .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+          .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+          .addField("Channel", message.channel)
+          .addField("Time", message.createdAt)
+          .addField("Reason", reason);
 
         //return message.channel.send(reportEmbed); //send msg in current channel
         let reportsChannel = message.guild.channels.find(`name`, "reports") //TODO: set reports channel
@@ -89,6 +107,47 @@ bot.on("message", async message => {
         message.delete().catch(O_o=>{}); //delete previous message (input command)
 
         return reportsChannel.send(reportEmbed);
+
+      //** Mod Commands **//
+      case `kick`:
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!kUser) message.channel.send("Can't find user!");
+        let kReason = args.join(" ").slice(22);
+
+        let kickEmbed = new discord.RichEmbed()
+          .setDescription("Reports")
+          .setColor("#ff4c00")
+          .addField("Kicked User", `${kUser} with ID: ${kUser.id}`)
+          .addField("Kicked By", `<@${message.author.id}> with ID: ${message.author.id}`)
+          .addField("Kicked From Channel", message.channel)
+          .addField("Time", message.createdAt)
+          .addField("Reason", kReason);
+          if(!message.member.hasPemission("MANAGE_MESSAGES")) return messages.channel.send("You don't have perms to kick. Report with `"+`${prefix}`+"`report 🙀")
+
+        let kickChannel = message.guild.channels.find(`name`, "incidents");
+        if (!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+        return;
+
+      case `ban`:
+      let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if (!kUser) message.channel.send("Can't find user!");
+      let bReason = args.join(" ").slice(22);
+
+      let banEmbed = new discord.RichEmbed()
+        .setDescription("Reports")
+        .setColor("#ff4c00")
+        .addField("Banned User", `${kUser} with ID: ${kUser.id}`)
+        .addField("Banned By", `<@${message.author.id}> with ID: ${message.author.id}`)
+        .addField("Kicked From Channel", message.channel)
+        .addField("Time", message.createdAt)
+        .addField("Reason", kReason);
+        if(!message.member.hasPemission("MANAGE_MESSAGES")) return messages.channel.send("You don't have perms to kick. Report with `"+`${prefix}`+"`report 🙀")
+
+      let banChannel = message.guild.channels.find(`name`, "incidents");
+      if (!banChannel) return message.channel.send("Can't find incidents channel.");
+
+      return;
 
       default:
         let botErr = new Discord.RichEmbed()
